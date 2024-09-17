826bbba5145cf8279e053ab0a18e6c59b3bde1c8
lunajin <luna7698@gmail.com>
2024-09-17 23:46:38 +0900

    router 설정

---
+++ src/layouts/TheView.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    <div class="container py-4">
+      <h2>Hello SangHyun's Project</h2>
+    </div>
+  </main>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
