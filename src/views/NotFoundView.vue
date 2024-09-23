c6f2ccc6de8b44715ea466109ff7778fba7d3608
lunajin <luna7698@gmail.com>
2024-09-23 23:04:40 +0900

    상현 수업

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="text-center py-5">
+    <h2>Oops!</h2>
+    <h2>404 Not Found</h2>
+    <div class="text-center">요청한 페이지를 찾을 수 없습니다.</div>
+    {{ $route.params.pathMatch }}
+    <div class="mt-4">
+      <router-link to="/">
+        <button class="btn btn-primary">Home</button>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
