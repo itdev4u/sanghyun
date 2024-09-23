adbe5f9f6b9da15445e9932fa0573b5c9dcccf3c
lunajin <luna7698@gmail.com>
2024-09-24 00:26:32 +0900

    수업

---
+++ src/components/AppCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="card">
+    <div v-if="$slots.header" class="card-head">
+      <slot name="header"></slot>
+    </div>
+    <div v-if="$slots.default" class="card-body">
+      <slot></slot>
+    </div>
+    <div v-if="$slots.footer" class="card-footer">
+      <slot name="footer"></slot>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
