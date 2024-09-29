16bdbf983312a2220bb85a9a331fe788afac22e7
lunajin <luna7698@gmail.com>
2024-09-29 23:12:03 +0900

    Day5

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,16 +1,8 @@
 // axios
 import axios from "axios";
 
-const posts = [
-  { id: 1, title: '제목1', content: '내용1', createdAt: '2024-09-15' },
-  { id: 2, title: '제목2', content: '내용2', createdAt: '2024-09-16' },
-  { id: 3, title: '제목3', content: '내용3', createdAt: '2024-09-17' },
-  { id: 4, title: '제목4', content: '내용4', createdAt: '2024-09-18' },
-  { id: 5, title: '제목5', content: '내용5', createdAt: '2024-09-19' },
-];
-
-export function getPosts() {
-  return axios.get('http://localhost:5000/posts');
+export function getPosts(params) {
+  return axios.get('http://localhost:5000/posts', { params });
 }
 
 export function getPostById(id) {
--- src/views/posts/PostListView.vue
+++ src/views/posts/PostListView.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
     <h2>게시글 목록</h2>
+    <form @submit.prevent>
+      <div class="row g-3">
+        <div class="col">
+          <input v-model="params.title_like" type="text" class="form-control" />
+        </div>
+        <div class="col">
+					<select v-model="params._limit" class="form-select">
+						<option value="3">3개씩 보기</option>
+						<option value="6">6개씩 보기</option>
+						<option value="9">9개씩 보기</option>
+					</select>
+        </div>
+      </div>
+    </form>
     <hr class="my-4">
     <div class="row g-3">
       <div v-for="post in posts" :key="post.id" class="col-4">
@@ -13,9 +27,23 @@
       </div>
     </div>
     <hr class="my-4"></hr>
-    <AppCard>
-      <PostDetailView id="1"></PostDetailView>
-    </AppCard>
+    <nav aria-label="Page navigation example">
+      <ul class="pagination justify-content-center">
+        <li class="page-item" :class="{ disabled: !( params._page > 1) }">
+          <a class="page-link" href="#" aria-label="Previous" @click.prevent="--params._page">
+            <span aria-hidden="true">&laquo;</span>
+          </a>
+        </li>
+        <li v-for="page in pageCount" :key="page" class="page-item" :class="{ active: params._page === page }">
+          <a class="page-link" href="#" @click.prevent="params._page = page">{{ page }}</a>
+        </li>
+        <li class="page-item" :class="{ disabled: !(params._page < pageCount) }">
+          <a class="page-link" href="#" aria-label="Next"  @click.prevent="++params._page">
+            <span aria-hidden="true">&raquo;</span>
+          </a>
+        </li>
+      </ul>
+    </nav>    
     
   </div>
 </template>
@@ -26,44 +54,36 @@ import PostDetailView from './PostDetailView.vue';
 import AppCard from '@/components/AppCard.vue';
 
 import { getPosts } from '@/api/posts';
-import { ref } from 'vue';
+import { ref, watchEffect } from 'vue';
 import { useRouter } from 'vue-router';
+import { computed } from 'vue';
 
 const posts = ref([]);
 const router = useRouter();
+// paging 관련 
+const params = ref({
+  _sort: 'createdAt',
+  _order: 'desc',
+  _limit: 3,
+  _page: 1,
+  title_like: ''
+});
 
-/* STEP 1 */
-/*
-const fetchPosts = () => {  
-  const response = getPosts();
-  console.log('response : ', response);
-  posts.value = getPosts();
-  promise 객체는 getPosts().then 으로 값을 받아야 한다.
-};
-*/
-/* STEP 1 END */
+const totalCount = ref(0);
+const pageCount = computed(() => Math.ceil(totalCount.value / params.value._limit));
+console.log('pageCount ==> ', pageCount.value);
+console.log('_limit ==> ', params.value._limit);
+console.log('totalCount ==> ', totalCount.value);
 
-/* STEP 2 */
-/*
-const fetchPosts = () => {
-  getPosts().then(response => {
-    console.log('response : ', response);
-  }).catch(error => {
-    console.error('Error : ', error);
-  });
-};
-*/
-/* STEP 2 END */
-
-/* STEP 3 */
 const fetchPosts = async () => {
-  const {data} = await getPosts();
+  const {data, headers } = await getPosts(params.value);
   posts.value = data;
-  console.dir(response);
+  totalCount.value = headers['x-total-count'];
 }
-/* STEP 3 END */
-fetchPosts();
 
+//fetchPosts();
+/* watchEffect에 callback 함수를 넣어주면 callback함수에서 사용하는 값이 변경되는 경우 함수를 재실행 해준다. */
+watchEffect(fetchPosts);
 const goPage = (id) => {
   router.push(`/posts/${id}`);
   //router.push('/posts/' + id);
