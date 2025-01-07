cc13f7c6b389fd71827f7cb6eefe5902ed1c4592
itdev4u <36702679+itdev4u@users.noreply.github.com>
2025-01-07 23:26:23 +0900

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,36 +1,37 @@
 import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue';
-import AboutView from '@/views/AboutView.vue';
-import PostCreateView from '@/views/posts/PostCreateView.vue';
-import PostDetailView from '@/views/posts/PostDetailView.vue';
-import PostEditView from '@/views/posts/PostEditView.vue';
-import PostListView from '@/views/posts/PostListView.vue';
-import NotFoundView from '@/views/NotFoundView.vue';
-import NestedView from '@/views/nested/NestedView.vue';
-import NestedOneView from '@/views/nested/NestedOneView.vue';
-import NestedTwoView from '@/views/nested/NestedTwoView.vue';
-import NestedHomeView from '@/views/nested/NestedHomeView.vue';
-import LoginView from '@/views/LoginView.vue';
+import HomeView from '@/views/HomeView.vue'
+import AboutView from '@/views/AboutView.vue'
+import PostCreateView from '@/views/posts/PostCreateView.vue'
+import PostDetailView from '@/views/posts/PostDetailView.vue'
+import PostEditView from '@/views/posts/PostEditView.vue'
+import PostListView from '@/views/posts/PostListView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
+import NestedView from '@/views/nested/NestedView.vue'
+import NestedOneView from '@/views/nested/NestedOneView.vue'
+import NestedTwoView from '@/views/nested/NestedTwoView.vue'
+import NestedHomeView from '@/views/nested/NestedHomeView.vue'
+import LoginView from '@/views/LoginView.vue'
 import TableView from '@/views/table/TableList.vue'
+import RegistView from '@/views/member/RegistView.vue'
 
 const routes = [
   {
     path: '/',
-    component: HomeView
+    component: HomeView,
   },
   {
     path: '/about',
-    component: AboutView
+    component: AboutView,
   },
   {
     path: '/posts',
     name: 'PostList',
-    component: PostListView
+    component: PostListView,
   },
   {
     path: '/posts/create',
     name: 'PostCreate',
-    component: PostCreateView
+    component: PostCreateView,
   },
   {
     path: '/posts/:id',
@@ -41,12 +42,12 @@ const routes = [
   {
     path: '/posts/:id/edit',
     name: 'PostEdit',
-    component: PostEditView
+    component: PostEditView,
   },
   {
     path: '/:pathMatch(.*)*',
     name: 'NotFound',
-    component: NotFoundView
+    component: NotFoundView,
   },
   {
     path: '/nested',
@@ -56,35 +57,40 @@ const routes = [
       {
         path: 'one',
         name: 'NestedOne',
-        component: NestedOneView
+        component: NestedOneView,
       },
       {
         path: 'two',
         name: 'NestedTwo',
-        component: NestedTwoView
+        component: NestedTwoView,
       },
       {
         path: '',
         name: 'NestedHome',
-        component: NestedHomeView
+        component: NestedHomeView,
       },
-    ]
+    ],
   },
   {
     path: '/login',
     name: 'Login',
-    component: LoginView
+    component: LoginView,
   },
   {
     path: '/table',
     name: 'Table',
-    component: TableView
-  }
-];
+    component: TableView,
+  },
+  {
+    path: '/regist',
+    name: 'Regist',
+    component: RegistView,
+  },
+]
 
 const router = createRouter({
   history: createWebHistory('/'),
-  routes: routes
-});
+  routes: routes,
+})
 
-export default router;
+export default router
